89a2e24c62b0534347fef6ca1d809c7a69396c5a
rayarafa8128-cpu <rayarafa8128@gmail.com>
2025-10-23 10:13:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1. pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementById('sapaButton
+//2. Tambahkan 'Event Listener' untuk aksi 'click'                                          
+tombolSapa.addEvenListener('click', function() {
+    //3. Jalankan kode ini ketika tombol di-Klik
+    alert('hallo! Terimakasih sudah berkunjung!');
+});
